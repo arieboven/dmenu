453b5afc47b30f5a54199cbfdbbd329243958355
aboven <arieboven@protonmail.com>
2021-10-01 20:02:00 +0200

    fix size with border width

--- dmenu.c
+++ dmenu.c
@@ -790,8 +790,8 @@ setup(void)
 					break;
 
 		if (centered) {
-			mw = MIN(MAX(max_textw() + promptw, min_width), info[i].width);
-			x = info[i].x_org + ((info[i].width  - mw) / 2);
+			mw = MIN(MAX(max_textw() + promptw, min_width), info[i].width) - (border_width * 2);
+			x = info[i].x_org + ((info[i].width  - mw) / 2) - border_width;
 			y = info[i].y_org + ((info[i].height - mh) / 2);
 		} else {
 			x = info[i].x_org;
